548baa9016d652af5de491d6997f7f400ac98a8a
Daniel E. Martinez Alvarez <117015704+Dema6495@users.noreply.github.com>
2025-01-20 17:50:04 +0000

    Update_Gimnasio.java
    Se ha creado un método para poder añadir mas actividades al Gimnasio.

--- Gimnasio.java
+++ Gimnasio.java
@@ -1,10 +1,18 @@
+package com.mycompany.gimnasio;
+import java.util.ArrayList;
+
 public class Gimnasio {
     private String nombre;
-    private String[] actividades = {"Yoga", "Boxeo", "Funcional"};
+    private String[] actividadesBasicas = {"Yoga", "Boxeo", "Funcional"};
+    private ArrayList<String> actividades = new ArrayList<>();
+    
 
     public Gimnasio(String nombre) {
         this.nombre = nombre;
-    }
+        for (int i = 0; i < actividadesBasicas.length; i++){
+            actividades.add(actividadesBasicas[i]);
+        }
+    }    
 
     public void mostrarActividades() {
         System.out.println("Actividades disponibles en el gimnasio " + nombre + ":");
@@ -16,10 +24,15 @@ public class Gimnasio {
     public void registrarUsuario(String usuario) {
         System.out.println("Usuario " + usuario + " registrado en el gimnasio " + nombre + ".");
     }
-
+    
+    public void nuevaActividad(String actividad){
+        actividades.add(actividad);
+    }
     public static void main(String[] args) {
         Gimnasio miGimnasio = new Gimnasio("FitLife Gym");
         miGimnasio.mostrarActividades();
         miGimnasio.registrarUsuario("Juan Perez");
+        miGimnasio.nuevaActividad("Baloncesto");
+        miGimnasio.mostrarActividades();
     }
 }
